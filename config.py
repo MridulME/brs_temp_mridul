7d8063bb97c37681f7dd7f8b61c75416e7c02aa5
MridulME <155382503+MridulME@users.noreply.github.com>
2024-09-23 14:30:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# My-SQL connection Details
+db_uri_conf = 'mysql+pymysql://infi:infi@127.0.0.1:3306/br_system'
+
+# MySql Table Details
+book_data_for_reader = 'book'
+user_data_for_reader = 'user_interest'
+
+
